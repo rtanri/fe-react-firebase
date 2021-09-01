c6c8c309183efc64698a2326c36c2afcdec28705
Revin Tanri <tanrirevin@gmail.com>
2021-09-01 22:42:14 +0800

    Add Skeleton during loading in ProposalPage, and render all proposal submitted data

--- src/pages/ProposalPage.jsx
+++ src/pages/ProposalPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { Redirect } from "react-router-dom";
-import { Button, Form, Input } from "antd";
+import { Skeleton } from "antd";
 import "firebase/firestore";
 import { getFirebaseInstance } from "../services/firebase/firebase";
 import { AuthContext } from "../components/AuthProvider";
@@ -34,23 +34,34 @@ function ProposalPage(props) {
       });
   }, [firestore, auth.authUserID]);
 
+  if (isLoading) {
+    return (<Skeleton active={ true } /> )
+  }
+
+  if (!proposalDoc) {
+    return (
+      <Redirect to='/submit-proposal' />
+    )
+  }
+
+// if user has register their proposal 1x, then can show this
   return (
     <div className="page-proposal container">
-      {!isLoading && proposalDoc ? (
-        <div
-          style={{
-            width: "100px",
-            maxWidth: "500px",
-            margin: "30px auto 0 auto",
-          }}
-        >
-          Proposal page
-        </div>
-      ) : (
-        <Redirect to="/submit-proposal" />
-      )}
+      <div style={{width:'100%', maxWidth:'500px', margin:'30px auto 0 auto'}}>
+        <h1>My Proposal</h1>
+        <p>Gallery Name : { proposalDoc.gallery_name } </p>
+        <p>Gallery Address : { proposalDoc.gallery_address } </p>
+        <p>Gallery Postal Code : {proposalDoc.gallery_postal_code} </p>
+        <ul>
+          {proposalDoc.artists.map(artist => {
+            return (
+              <li>{artist}</li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default ProposalPage;
